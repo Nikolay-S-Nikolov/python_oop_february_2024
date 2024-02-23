cbe0aca0d93a3e06642b2af89ec945c0f504bb5c
nikolasy.s.nikolov@gmail.com <nikolay.s.nikolov@gmail.com>
2024-02-23 20:13:54 +0200

    Problem 01. Rhombus of Stars

---
+++ 03_first_steps_in_oop/1_rhombus_of_stars.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+
+def print_row(size, row):
+    print(' ' * (size - row), end='')
+    print('* ' * row)
+
+
+def print_upper_part(size):
+    for row in range(1, size + 1):
+        print_row(size, row)
+
+
+def print_lower_part(size):
+    for row in range(size - 1, 0, -1):
+        print_row(size, row)
+
+
+def print_rhombus(size):
+    print_upper_part(size)
+    print_lower_part(size)
+
+
+print_rhombus(n)
