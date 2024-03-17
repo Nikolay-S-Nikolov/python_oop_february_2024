dd20775808ad98fc0eceae78aa49d1d86a636673
Nikolay-S-Nikolov <nikolay.s.nikolov@gmail.com>
2024-03-17 22:22:42 +0200

    Problem solution added

--- 24_exam_preparation/04_python_oop_retake_exam_22_august_2022/03_unit_testing/project/test/test_shopping.py
+++ 24_exam_preparation/04_python_oop_retake_exam_22_august_2022/03_unit_testing/project/test/test_shopping.py
@@ -31,12 +31,46 @@ class TestShoppingCart(TestCase):
             self.cart.add_to_cart('apple', 100)
         self.assertEqual("Product apple cost too much!", str(ve.exception))
 
-    def test_add_to_cart_method_correct_price_raise_return_message(self):
+    def test_add_to_cart_method_with_correct_price_raise_return_message(self):
         result = self.cart.add_to_cart('apple', 5)
         self.assertEqual("apple product was successfully added to the cart!", result)
         self.assertEqual({'apple': 5}, self.cart.products)
 
+    def test_remove_from_cart_with_not_existing_product_raises_value_error(self):
+        with self.assertRaises(ValueError) as ve:
+            self.cart.remove_from_cart("test")
+        self.assertEqual("No product with name test in the cart!", str(ve.exception))
 
+    def test_remove_from_cart_with_existing_product_return_message(self):
+        self.cart.add_to_cart("apple", 7)
+        self.cart.add_to_cart("melon", 8)
+        result = self.cart.remove_from_cart('apple')
+        self.assertEqual("Product apple was successfully removed from the cart!", result)
+        self.assertEqual({"melon": 8}, self.cart.products)
+
+    def test__add__method_correct_functionality(self):
+        new_cart = ShoppingCart("City", 200)
+        new_cart.products = {"Lemon": 3, "Watermelon": 4}
+        self.cart.products = {"Apple": 3, "Banana": 4}
+        result = self.cart + new_cart
+        self.assertEqual("LidlCity", result.shop_name)
+        self.assertEqual(300, result.budget)
+        self.assertEqual({'Apple': 3, 'Banana': 4, 'Lemon': 3, 'Watermelon': 4}, result.products)
+
+    def test_buy_products_method_with_total_sum_bigger_than_budget_raises_value_error(self):
+        new_cart = ShoppingCart('Store', 10)
+        new_cart.products = {'Apple': 3, 'Banana': 4, 'Lemon': 3, 'Watermelon': 4}
+        expected = f"Not enough money to buy the products! Over budget with {4:.2f}lv!"
+        with self.assertRaises(ValueError) as ve:
+            new_cart.buy_products()
+        self.assertEqual(expected, str(ve.exception))
+
+    def test_buy_products_method_return_correct_message(self):
+        new_cart = ShoppingCart('Store', 15)
+        new_cart.products = {'Apple': 3, 'Banana': 5, 'Lemon': 3, 'Watermelon': 4}
+        result = new_cart.buy_products()
+        expected = f'Products were successfully bought! Total cost: {15:.2f}lv.'
+        self.assertEqual(expected, result)
 
 
 if __name__ == "__main__":
