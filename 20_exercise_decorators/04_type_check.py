e61ecb05724812e2a489edc5cf54b08e47fc9a5a
Nikolay-S-Nikolov <nikolay.s.nikolov@gmail.com>
2024-05-05 11:19:55 +0300

    Problem solution added

---
+++ 20_exercise_decorators/04_type_check.py
@@ -0,0 +1,33 @@
+def type_check(type):
+    def decorator(func):
+        def wrapper(arg):
+            return func(arg) if isinstance(arg, type) else 'Bad Type'
+
+        return wrapper
+
+    return decorator
+
+
+@type_check(int)
+def times2(num):
+    return num * 2
+
+
+print(times2(2))
+print(times2('Not A Number'))
+
+
+# Output
+# 4
+# Bad Type
+
+@type_check(str)
+def first_letter(word):
+    return word[0]
+
+
+print(first_letter('Hello World'))
+print(first_letter(['Not', 'A', 'String']))
+# Output
+# H
+# Bad Type
